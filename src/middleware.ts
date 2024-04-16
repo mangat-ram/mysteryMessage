1b97e4d2e81ea63e3cc301755d33ad237523bca9
mangat-ram <se.mangat.ram@gmail.com>
2024-04-16 23:55:17 +0530

    Progress Commit MiddleWare.

---
+++ src/middleware.ts
@@ -0,0 +1,26 @@
+import { NextResponse } from "next/server"
+import type { NextRequest } from "next/server"
+export { default } from "next-auth/middleware"
+import { getToken } from "next-auth/jwt"
+
+// This function can be marked `async` if using `await` inside
+export async function middleware(request: NextRequest) {
+
+  const token = await getToken({req: request});
+  const url = request.nextUrl;
+
+  
+
+  return NextResponse.redirect(new URL('/home', request.url))
+}
+ 
+// See "Matching Paths" below to learn more
+export const config = {
+  matcher: [
+    '/sign-in',
+    '/sign-up',
+    '/',
+    '/dashboard/:path*',
+    '/verify/:path*'
+  ],
+}
