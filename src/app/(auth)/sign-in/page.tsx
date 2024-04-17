709fd992e57b913c55e1624b64c62d2e3f14b0c0
mangat-ram <se.mangat.ram@gmail.com>
2024-04-17 19:57:31 +0530

    Added Sample SignIn Component.

--- src/app/(auth)/sign-in/page.tsx
+++ src/app/(auth)/sign-in/page.tsx
@@ -1,9 +1,21 @@
-import React from 'react'
+"use client";
 
-const SignIn = () => {
+import { useSession, signIn, signOut } from "next-auth/react"
+
+export default function Component() {
+  const { data: session } = useSession()
+  if (session) {
+    return (
+      <>
+        Signed in as {session.user.email} <br />
+        <button onClick={() => signOut()}>Sign out</button>
+      </>
+    )
+  }
   return (
-    <div>SignIn</div>
+    <>
+      Not signed in <br />
+      <button onClick={() => signIn()}>Sign in</button>
+    </>
   )
 }
-
-export default SignIn;
---
+++ src/context/AuthProviders.tsx
@@ -0,0 +1,11 @@
+"use client";
+
+import React from 'react'
+
+const AuthProviders = () => {
+  return (
+    <div>AuthProviders</div>
+  )
+}
+
+export default AuthProviders
