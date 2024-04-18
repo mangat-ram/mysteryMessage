e9ee195043a3948fe070733a8f054e4cfb385659
mangat-ram <se.mangat.ram@gmail.com>
2024-04-18 22:46:19 +0530

    Added Accept Messages API.

---
+++ src/app/api/acceptMessages/route.ts
@@ -0,0 +1,51 @@
+import dbConnect from "@/lib/dbConnect";
+import { User, getServerSession } from "next-auth";
+import { authOptions } from "../auth/[...nextauth]/options";
+import UserModel from "@/models/User.model";
+
+export async function POST(request: Request){
+  await dbConnect();
+  const session = await getServerSession(authOptions);
+  const user: User = session?.user as User;
+  if(!session || !session?.user){
+    return Response.json(
+        {
+          success:false,
+          message: "Not Authenticated"
+        },{status:401}
+      )
+  }
+  const userId = user._id;
+  const { acceptMessages } = await request.json();
+  try {
+    const updatedUser = await UserModel.findById(
+          userId,
+          {isAcceptingMessage : acceptMessages},
+          {new : true}
+        )
+    if(!updatedUser){
+      return Response.json(
+        {
+          success:false,
+          message: "Updated User Not Found."
+        },{status:404}
+      )
+    }else{
+      return Response.json(
+          {
+            success:false,
+            message: "Updated User Not Found."
+          },{status:404}
+      )
+    }
+    
+  } catch (error) {
+    return Response.json(
+        {
+          success:false,
+          message: "Failed to update User status to accept Messages."
+        },{status:500}
+      )
+  }
+
+}
