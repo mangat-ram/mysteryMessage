309d433f9c0720f9a8896b305bbba18b23e51d45
mangat-ram <se.mangat.ram@gmail.com>
2024-04-18 22:04:07 +0530

    Added Unique User API fix issue.

--- src/app/api/checkUniqueUser/route.ts
+++ src/app/api/checkUniqueUser/route.ts
@@ -41,7 +41,7 @@ export async function GET(request: Request){
         {
           success: false,
           message: "username is available"
-        },{status: 400}
+        },{status: 200}
       )
 
   } catch (error) {
