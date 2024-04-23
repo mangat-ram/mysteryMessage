f379e7ca83ac6ff3b409a91a94381bdc38d126b1
mangat-ram <se.mangat.ram@gmail.com>
2024-04-23 21:12:47 +0530

    Added Axios.

--- src/app/(auth)/signIn/page.tsx
+++ src/app/(auth)/signIn/page.tsx
@@ -1,21 +1,16 @@
 "use client";
 
-import { useSession, signIn, signOut } from "next-auth/react"
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useForm } from "react-hook-form";
+import Link from "next/link";
+import * as z from "zod";
 
-export default function Component() {
-  const { data: session } = useSession()
-  if (session) {
-    return (
-      <>
-        Signed in as {session.user.email} <br />
-        <button onClick={() => signOut()}>Sign out</button>
-      </>
-    )
-  }
+import React from 'react'
+
+const Page = () => {
   return (
-    <>
-      Not signed in <br />
-      <button className="px-3 py-2 text-xl rounded-lg bg-amber-700 text-white" onClick={() => signIn()}>Sign in</button>
-    </>
+    <div>Page</div>
   )
 }
+
+export default Page
