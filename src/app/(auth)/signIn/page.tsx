3fb5872add3d3dd0762b331619902727a57870d1
mangat-ram <se.mangat.ram@gmail.com>
2024-04-23 21:54:02 +0530

    Added API Get Request for checking user name by useEffect.

--- src/app/(auth)/signIn/page.tsx
+++ src/app/(auth)/signIn/page.tsx
@@ -5,8 +5,12 @@ import { useForm } from "react-hook-form";
 import Link from "next/link";
 import * as z from "zod";
 import { useDebounceValue } from "usehooks-ts";
-
-import React, { useState } from 'react'
+import { useToast } from "@/components/ui/use-toast";
+import axios,{AxiosError} from "axios";
+import React, { useEffect, useState } from 'react'
+import { useRouter } from "next/navigation";
+import { signUpSchema } from "@/schemas/signUpSchema";
+import { ApiResponse } from "@/types/ApiResponse";
 
 const Page = () => {
 
@@ -14,7 +18,41 @@ const Page = () => {
   const [usernameMessage, setUsernameMessage] = useState("");
   const [isCheckingUsername, setIsCheckingUsername] = useState(false);
   const [isSubmitting,setIsSubmitting] = useState(false);
+
   const debouncedUsername = useDebounceValue(username,300);
+  const { toast } = useToast();
+  const router = useRouter();
+
+  const form = useForm<z.infer<typeof signUpSchema>>({
+    resolver: zodResolver(signUpSchema),
+    defaultValues: {
+      username:"",
+      email:"",
+      password:""
+    }
+  })
+
+  useEffect(() => {
+    const checkUsernameUnique = async () => {
+      if(debouncedUsername){
+        setIsCheckingUsername(true);
+        setUsernameMessage("");
+        try {
+          const res = await axios.get(`/api/checkUniqueUser?username=${debouncedUsername}`)
+          setUsernameMessage(res.data.message)
+        } catch (error) {
+          const axiosError = error as AxiosError<ApiResponse>;
+          setUsernameMessage(
+            axiosError.response?.data.message ?? "Error Checking Username"
+          )
+        } finally {
+          setIsCheckingUsername(false);
+        }
+      }
+    }
+    checkUsernameUnique();
+  }
+  , [debouncedUsername])
 
   return (
     <div>Page</div>
