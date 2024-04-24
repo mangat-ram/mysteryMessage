e308ffd95a5c7c7f7d29439acd022991c5e3a83d
mangat-ram <se.mangat.ram@gmail.com>
2024-04-24 21:20:47 +0530

    Fixed issue in sign in Schema.

--- src/app/(auth)/signIn/page.tsx
+++ src/app/(auth)/signIn/page.tsx
@@ -1,20 +1,104 @@
+import { useToast } from '@/components/ui/use-toast';
 import { signInSchemaVal } from '@/schemas/signInSchema';
 import { zodResolver } from '@hookform/resolvers/zod';
+import { signIn } from 'next-auth/react';
 import { useRouter } from 'next/navigation'
-import React from 'react'
+import React, { useState } from 'react'
 import { useForm } from 'react-hook-form';
 import * as z from 'zod';
+import { Button } from "@/components/ui/button"
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form"
+import { Input } from "@/components/ui/input"
+import { Loader2 } from 'lucide-react';
 
 const SignIn = () => {
 
+  const [isSubmitting, setIsSubmitting] = useState(false);
   const router = useRouter();
+  const { toast } = useToast();
   // const {username,password} = 
   const form = useForm<z.infer<typeof signInSchemaVal>>({
-    resolver: zodResolver(signInSchemaVal)
+    resolver: zodResolver(signInSchemaVal),
+    defaultValues: {
+      identifier:'',
+      password:''
+    }
   })
 
+  const submitLogin = async (data: z.infer<typeof signInSchemaVal>) => {
+    await signIn('credentials',{
+      identifier: data.identifier,
+      password: data.password
+    })
+  }
+
+
   return (
-    <div>SignIn</div>
+        <div className="flex justify-center items-center min-h-screen bg-gray-100">
+      <div className="w-full max-w-md p-8 space-y-8 bg-white rounded-lg shadow-md">
+        <div className="text-center">
+          <h1 className="text-4xl font-extrabold tracking-tight lg:text-5xl mb-6">
+            Log In
+          </h1>
+          <p className="mb-4">
+            enter email and password to Sign In
+          </p>
+        </div>
+        <Form {...form}>
+          <form onSubmit={form.handleSubmit(submitLogin)} className="space-y-6">
+            <FormField
+              control={form.control}
+              name="identifier"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Email</FormLabel>
+                  <FormControl>
+                    <Input 
+                      placeholder="your email" 
+                      {...field}
+                    />
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+            <FormField
+              control={form.control}
+              name="password"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Password</FormLabel>
+                  <FormControl>
+                    <Input 
+                      type="password"
+                      placeholder="password" 
+                      {...field}
+                    />
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+            <Button type="submit" disabled={isSubmitting}>
+              {
+                  isSubmitting ? (<>
+                    <Loader2 
+                      className="mr-2 h-4 w-4 animate-spin" 
+                    /> Please wait
+                  </>) : ("SignIn")
+              }
+            </Button>
+          </form>
+        </Form>
+      </div>
+    </div>
   )
 }
 
--- src/schemas/signInSchema.ts
+++ src/schemas/signInSchema.ts
@@ -1,6 +1,6 @@
 import { z } from "zod";
 
 export const signInSchemaVal = z.object({
-  username: z.string(),
+  identifier: z.string(),
   password: z.string()
 })
