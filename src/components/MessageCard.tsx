26f98cec8af7ce13868814c3fe6e686c54539371
mangat-ram <se.mangat.ram@gmail.com>
2024-04-25 21:47:49 +0530

    Addded Delete Function.

--- src/components/MessageCard.tsx
+++ src/components/MessageCard.tsx
@@ -7,13 +7,59 @@ import {
   CardHeader,
   CardTitle,
 } from "@/components/ui/card"
+import {
+  AlertDialog,
+  AlertDialogAction,
+  AlertDialogCancel,
+  AlertDialogContent,
+  AlertDialogDescription,
+  AlertDialogFooter,
+  AlertDialogHeader,
+  AlertDialogTitle,
+  AlertDialogTrigger,
+} from "@/components/ui/alert-dialog"
+import { X } from "lucide-react"
+import { MessageSchemaType } from "@/models/User.model"
+import { useToast } from "./ui/use-toast"
+import axios from "axios"
+import { ApiResponse } from "@/types/ApiResponse"
 
+type MessageCardProps = {
+  message: MessageSchemaType;
+  onMessageDelete: (messageId: string) => void
+}
 
-const MessageCard = () => {
+const MessageCard = ({message,onMessageDelete} : MessageCardProps) => {
+
+  const { toast } = useToast();
+  const handleDeleteConfirm = async () => {
+    const res = await axios.delete<ApiResponse>(`/api/deleteMessage/${message._id}`);
+    toast({
+      title: res.data.message
+    })
+    onMessageDelete(message._id)
+  }
+ 
   return (
     <Card>
       <CardHeader>
         <CardTitle>Card Title</CardTitle>
+        <AlertDialog>
+          <AlertDialogTrigger><X className="w-5 h-5" /></AlertDialogTrigger>
+            <AlertDialogContent>
+              <AlertDialogHeader>
+                <AlertDialogTitle>Are you absolutely sure?</AlertDialogTitle>
+                <AlertDialogDescription>
+                  This action cannot be undone. This will permanently delete your account
+                  and remove your data from our servers.
+                </AlertDialogDescription>
+              </AlertDialogHeader>
+              <AlertDialogFooter>
+                <AlertDialogCancel>Cancel</AlertDialogCancel>
+                <AlertDialogAction onClick={handleDeleteConfirm}>Continue</AlertDialogAction>
+              </AlertDialogFooter>
+            </AlertDialogContent>
+          </AlertDialog>
         <CardDescription>Card Description</CardDescription>
       </CardHeader>
       <CardContent>
