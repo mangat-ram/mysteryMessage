51fad15701158c5246f8b08422f2873f43e23599
mangat-ram <se.mangat.ram@gmail.com>
2024-04-25 21:05:45 +0530

    completed Navbar.

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -4,14 +4,32 @@ import React from "react";
 import Link from "next/link";
 import { useSession, signOut } from "next-auth/react";
 import { User } from "next-auth";
+import { Button } from "./ui/button";
 
 const NavBar = () => {
 
   const { data:session } = useSession();
-  const user: User = session?.user
+  const user = session?.user as User
 
   return (
-    <div>NavBar</div>
+    <nav className="p-4 md:p-6 shadow-md">
+      <div className="container mx-auto flex flex-col md:flex-row justify-center items-center">
+        <a className="text-xl font-bold mb-4 md:mb-0" href="#">Mystery Message</a>
+        { 
+          session ? (
+            <>
+              <span className="mr-4">
+              welcome {user?.username || user?.email}
+              </span>
+              <Button className="w-full md:w-auto" onClick={() => signOut()}>Logout</Button>
+            </>
+          ): (
+            <Link href="/signIn">
+              <Button className="w-full md:w-auto">Login</Button>
+            </Link>
+          )}
+      </div>
+    </nav>
   )
 }
 
