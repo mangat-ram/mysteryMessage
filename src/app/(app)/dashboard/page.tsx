657adfae4875aecf03604f41575c459d9a49e8eb
mangat-ram <se.mangat.ram@gmail.com>
2024-04-28 12:46:33 +0530

    Added Dashboard Completed.

--- src/app/(app)/dashboard/page.tsx
+++ src/app/(app)/dashboard/page.tsx
@@ -1,11 +1,17 @@
 "use client";
 
+import MessageCard from "@/components/MessageCard";
+import { Button } from "@/components/ui/button";
+import { Separator } from "@/components/ui/separator";
+import { Switch } from "@/components/ui/switch";
 import { useToast } from "@/components/ui/use-toast";
 import { MessageSchemaType } from "@/models/User.model"
 import { acceptMessagesSchemaVal } from "@/schemas/acceptMessageSchema";
 import { ApiResponse } from "@/types/ApiResponse";
 import { zodResolver } from "@hookform/resolvers/zod";
 import axios, { AxiosError } from "axios";
+import { Loader2, RefreshCcw } from "lucide-react";
+import { User } from "next-auth";
 import { useSession } from "next-auth/react";
 import React, { useCallback, useEffect, useState } from "react"
 import { useForm } from "react-hook-form";
@@ -71,7 +77,7 @@ const Dashboard = () => {
       setIsLoading(false)
       setIsSwitchLoading(false)
     }
-  },[setIsLoading, setMessages])
+  },[setIsLoading, setMessages, toast])
 
   useEffect(() => {
     if(!session || !session.user) return;
@@ -79,11 +85,101 @@ const Dashboard = () => {
     fetchAcceptMessages()
   },[session,setValue,fetchAcceptMessages,fetchMessages])
 
-  
+  const handleSwitch = async () => {
+    try {
+      const res = await axios.post<ApiResponse>('/api/acceptMessages',{acceptMessages:!acceptMessages});
+      setValue('acceptMessages',!acceptMessages)
+      toast({
+        title: res.data.message,
+        variant:"default"
+      })
+    } catch (error) {
+      const axiosError = error as AxiosError<ApiResponse>
+      toast({
+        title: "Error",
+        description:"Failed to switch message status",
+        variant:"destructive"
+      })
+    }
+  }
+
+  const { username } = session?.user as User
+  const baseUrl = `${window.location.protocol}//${window.location.host}`
+  const profileUrl = `${baseUrl}/u/${username}`
+  const copyToClipboard = () => {
+    navigator.clipboard.writeText(profileUrl)
+    toast({
+      title:"URL copied",
+      description:"Profile url has been copied to clipboard"
+    })
+  }
+
+  if(!session || !session.user){
+    return (
+      <div>Please Login</div>
+    )
+  }
 
   return (
-    <div>Dashboard</div>
-  )
+    <div
+      className="my-8 mx-4 md:mx-8 lg:mx-auto p-6 bg-white rounded w-full max-w-6xl"
+    >
+      <h1 className="text-4xl font-bold mb-4">
+        User Dashboard
+      </h1>
+      <div className="mb-4">
+        <h2 className="text-lg font-semibold mb-2">Copy Your Unique Link</h2>{" "}
+        <div className="flex items-center">
+          <input
+            type="text"
+            value={profileUrl}
+            disabled
+            className="input input-bordered w-full p-2 mr-2"
+          />
+          <Button onClick={copyToClipboard}>Copy</Button>
+        </div>
+      </div>
+      <div className="mb-4">
+        <Switch
+          {...register('acceptMessages')}
+          checked={acceptMessages}
+          onCheckedChange={handleSwitch}
+          disabled={isSwitchLoading}
+        />
+        <span className="ml-2">
+          Accept Messages: {acceptMessages ? "On" : "Off"}
+        </span>
+      </div>
+      <Separator />
+      <Button
+        className="mt-4"
+        variant="outline"
+        onClick={(e) => {
+          e.preventDefault();
+          fetchMessages(true);
+        }}
+      >
+        {isLoading ? (
+          <Loader2 className="h-4 w-4 animate-spin" />
+        ) : (
+          <RefreshCcw className="h-4 w-4" />
+        )}
+      </Button>
+      <div className="mt-4 grid grid-cols-1 md:grid-cols-2 gap-6">
+        {messages.length > 0 ? (
+          messages.map((message,index) => (
+            <MessageCard 
+              key={message._id}
+              message={message}
+              onMessageDelete={handleDeleteMsg}
+            />
+          ))
+        ) : (
+          <p>No messages To Display.</p>
+        )}
+      </div>
+    </div>
+  );
 }
 
 export default Dashboard
