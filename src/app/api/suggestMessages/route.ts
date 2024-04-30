b21bdfe36eb2d89944406eaf8ef18fe7f7874bf5
mangat-ram <se.mangat.ram@gmail.com>
2024-04-30 22:42:43 +0530

    Modified the Suggest Messages route.

--- src/app/api/suggestMessages/route.ts
+++ src/app/api/suggestMessages/route.ts
@@ -1,5 +1,6 @@
 import OpenAI from 'openai';
 import { OpenAIStream, StreamingTextResponse } from 'ai';
+import { NextResponse } from 'next/server';
  
 // Create an OpenAI API client (that's edge friendly!)
 const openai = new OpenAI({
@@ -9,17 +10,30 @@ const openai = new OpenAI({
 export const dynamic = 'force-dynamic';
  
 export async function POST(req: Request) {
-  const { messages } = await req.json();
- 
-  // Ask OpenAI for a streaming chat completion given the prompt
-  const response = await openai.chat.completions.create({
-    model: 'gpt-3.5-turbo',
-    stream: true,
-    messages,
-  });
- 
-  // Convert the response into a friendly text-stream
-  const stream = OpenAIStream(response);
-  // Respond with the stream
-  return new StreamingTextResponse(stream);
+  try {
+    const prompt =  "Create a list of three open-ended and engaging questions formatted as a single string. Each question should be separated by '||'. These questions are for an anonymous social messaging platform, like Qooh.me, and should be suitable for a diverse audience. Avoid personal or sensitive topics, focusing instead on universal themes that encourage friendly interaction. For example, your output should be structured like this: 'What’s a hobby you’ve recently started?||If you could have dinner with any historical figure, who would it be?||What’s a simple thing that makes you happy?'. Ensure the questions are intriguing, foster curiosity, and contribute to a positive and welcoming conversational environment.";
+   
+    // Ask OpenAI for a streaming chat completion given the prompt
+    const response = await openai.completions.create({
+      model: 'gpt-3.5-turbo-instruct',
+      max_tokens: 100,
+      stream: true,
+      prompt,
+    });
+   
+    // Convert the response into a friendly text-stream
+    const stream = OpenAIStream(response);
+    // Respond with the stream
+    return new StreamingTextResponse(stream);
+  } catch (error) {
+    if(error instanceof OpenAI.APIError){
+      const {name, status, headers, message} = error
+      return NextResponse.json({
+        name,status,headers,message
+      }, {status})
+    }else{
+      console.error("An unexpected error occurred ",error)
+      throw error;
+    }
+  }
 }
