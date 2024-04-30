3c84d21c12421ccdd765cf67ce915617198e99ab
mangat-ram <se.mangat.ram@gmail.com>
2024-04-30 21:22:40 +0530

    Fixed Next Auth CredentialsProvider Issue.

--- src/app/api/auth/[...nextauth]/options.ts
+++ src/app/api/auth/[...nextauth]/options.ts
@@ -1,6 +1,5 @@
 import { NextAuthOptions } from "next-auth";
-import Credentials from "next-auth/providers/credentials";
-import { CredentialsProvider } from "next-auth/providers/credentials";
+import CredentialsProvider from "next-auth/providers/credentials";
 import bcrypt from "bcryptjs"
 import dbConnect from "@/lib/dbConnect";
 import UserModel from "@/models/User.model";
