e8864b1adc45ff943086bb82b1fc7b9ed532e977
mangat-ram <se.mangat.ram@gmail.com>
2024-04-30 21:23:19 +0530

    Fixed Gmail Issue.

--- src/helpers/sendVerificationEmail.ts
+++ src/helpers/sendVerificationEmail.ts
@@ -9,7 +9,7 @@ export async function sendVerificationEmail(
 ): Promise<ApiResponse>{
   try {
     await resend.emails.send({
-      from: 'dev@hiteshchoudhary.com',
+      from: 'se.mangat.ram@gmail.com',
       to: email,
       subject: 'Mystery Message Verification Code',
       react: VerificationEmail({ username, otp: verifyCode }),
